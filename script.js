6d53ebdac26f621ac355737bc8caa6495e1be9ba
Hanan Nasir Ahmed <148802673+hann2004@users.noreply.github.com>
2024-10-26 13:58:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -48,4 +48,24 @@ function toggleMoreText() {
         seeMore.textContent = "Read More";
     }
     }
+
+ let menuli = document.querySelectorAll('header ul li a');
+    let section = document.querySelectorAll('section'); 
+    
+    function activeMenu() {
+        let len = section.length;
+        while (--len && window.scrollY + 97 < section[len].offsetTop) {}
+        menuli.forEach(sec => sec.classList.remove("active"));
+        menuli[len].classList.add("active");
+    }
+    
+    activeMenu();
+    window.addEventListener("scroll", activeMenu);
+    
+    const header = document.querySelector("header");
+    window.addEventListener("scroll", function(){
+        header.classList.toggle("sticky", window.scrollY > 50);
+        console.log(header.classList.contains("sticky")); 
+    });
+    
   
